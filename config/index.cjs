583ddbd82935c46f579c6cf50575420654a35ccd
zyain0329 <127910932+zyain0329@users.noreply.github.com>
2023-03-15 10:58:01 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -52,13 +52,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'o5P8j6fw9cgA1yn3REm0DTs63NLE',
+  CALLBACK_TEMPLATE_ID: '97c9f0fdc985478c9b27217a0081e671',
 
   CALLBACK_USERS: [
     {
       name: '自己',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'o5P8j6fw9cgA1yn3REm0DTs63NLE',
+      id: '97c9f0fdc985478c9b27217a0081e671',
     }
   ],
 
