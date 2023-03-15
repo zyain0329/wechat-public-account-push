aa2fd8e7f4727a821ef3fc9f660a2a39e1811fb6
zyain0329 <127910932+zyain0329@users.noreply.github.com>
2023-03-15 11:29:39 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -5,8 +5,7 @@
  * 如若想使用更多功能，请查考文档中的 【3. config参数说明】 
  * 自行添加属性，以支持更多个性化功能
  */
-const USER_CONFIG = {
-
+const USER_CONFIG = [
   // 使用微信测试号：公众号APP_ID
   APP_ID: 'wx9667b250a07c091f',
 
@@ -34,18 +33,15 @@ const USER_CONFIG = {
         {
           type: '节日', name: '相识纪念日', year: '2016', date: '08-01',
         },
-      ],
-      // 我们在一起已经有xxxx天了的配置
-      customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2016-08-01' },
-      ],
+        ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'o5P8j6fw9cgA1yn3REm0DTs63NLE',
 
   CALLBACK_USERS: [
     {
